8ab47562657d5cf934d4d3ffc6a580825c2b78f5
Vinay Shetye <vinay.courses.official@gmail.com>
2024-08-06 21:56:50 -0400

    Updated Files

---
+++ src/components/PlayScreen/PlayScreenWrapper.js
@@ -0,0 +1,194 @@
+import styled from "styled-components";
+
+const PlayScreenWrapper = styled.div`
+  .game_bar {
+    background: rgba(255, 255, 255, 0.3);
+    border-radius: 16px;
+    backdrop-filter: blur(10.8px);
+    -webkit-backdrop-filter: blur(10.8px);
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px,
+      rgba(0, 0, 0, 0.3) 0px 7px 13px -3px,
+      rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
+  }
+
+  .answer_field {
+    box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+      rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px,
+      rgba(0, 0, 0, 0.3) 0px 7px 13px -3px,
+      rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
+  }
+
+  .answer_field::placeholder {
+    text-transform: none;
+  }
+
+  .play_box {
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+      rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
+      rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+  }
+
+  .word_box {
+    background: rgba(255, 255, 255, 0.26);
+    border-radius: 16px;
+    backdrop-filter: blur(16.7px);
+    -webkit-backdrop-filter: blur(16.7px);
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px,
+      rgba(0, 0, 0, 0.3) 0px 7px 13px -3px,
+      rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
+  }
+
+  @media (max-width: 900px) {
+    .navbar {
+      padding: 0;
+      justify-content: space-between;
+      background: rgba(255, 255, 255, 0.3);
+      box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px,
+        rgba(0, 0, 0, 0.3) 0px 7px 13px -3px,
+        rgba(0, 0, 0, 0.2) 0px -4px 0px inset;
+    }
+
+    .menu_btn,
+    .menu_btn_icon {
+      margin: 0;
+    }
+
+    .menu_btn,
+    .menu_btn_text {
+      border-radius: 0;
+    }
+
+    .btn_label {
+      display: none;
+    }
+
+    .game_bar {
+      width: 100%;
+      margin: 0;
+      box-shadow: none;
+      justify-content: flex-end;
+      border-radius: 0;
+      background-color: transparent;
+      backdrop-filter: blur(0px);
+      padding: 16px;
+    }
+  }
+
+  @media (max-width: 700px) {
+    .game_bar_icon {
+      display: none;
+    }
+  }
+
+  @media (max-width: 650px) {
+    .play_box {
+      padding: 38px 28px 30px 28px;
+    }
+
+    .play_box h1 {
+      font-size: 50px;
+      margin-bottom: 25px;
+    }
+
+    .word_box {
+      padding: 30px 25px;
+      margin-bottom: 25px;
+    }
+
+    .word_box p {
+      font-size: 40px;
+    }
+
+    .input_wrapper {
+      flex-direction: column;
+    }
+
+    .input_wrapper .submit_btn {
+      display: block;
+      margin: 25px auto 0;
+      max-width: fit-content;
+    }
+
+    @media (max-width: 540px) {
+      .navbar {
+      }
+
+      .game_bar {
+        padding: 6px 15px;
+      }
+
+      .game_bar span {
+        font-size: 20px;
+      }
+
+      .menu_btn_text {
+        padding: 12px;
+      }
+
+      .menu_btn_icon {
+        width: 20px;
+        height: 20px;
+      }
+    }
+
+    @media (max-width: 506px) {
+      .play_box h1 {
+        font-size: 40px;
+      }
+
+      .word_box {
+        padding: 30px 25px;
+        margin-bottom: 25px;
+      }
+
+      .word_box p {
+        font-size: 30px;
+      }
+
+      .space-x-5px > :not([hidden]) ~ :not([hidden]) {
+        --tw-space-x-reverse: 0;
+        margin-right: calc(16px * var(--tw-space-x-reverse));
+        margin-left: calc(16px * calc(1 - var(--tw-space-x-reverse)));
+      }
+
+      .submit_btn_text,
+      .upload_btn_text {
+        font-size: 20px;
+        padding: 13px 20px;
+      }
+    }
+
+    @media (max-width: 430px) {
+      .play_box {
+        padding: 24px;
+      }
+
+      .play_box h1 {
+        font-size: 28px;
+        margin-bottom: 25px;
+      }
+
+      .word_box p {
+        font-size: 24px;
+      }
+
+      .input_wrapper input {
+        font-size: 18px;
+        padding: 12px;
+      }
+
+      .submit_btn_text,
+      .upload_btn_text {
+        font-size: 16px;
+        padding: 8px 18px;
+      }
+
+      .game_bar span {
+        font-size: 16px;
+      }
+    }
+  }
+`;
+
+export default PlayScreenWrapper;
---
+++ src/contexts/MusicContext.jsx
@@ -0,0 +1,63 @@
+// src/contexts/MusicContext.jsx
+
+import React, {
+  createContext,
+  useState,
+  useContext,
+  useCallback,
+  useRef,
+} from "react";
+import { BgMusic1 } from "../assets/assets";
+
+const MusicContext = createContext();
+
+export const MusicProvider = ({ children }) => {
+  const [isMusicOn, setIsMusicOn] = useState(true);
+  const [isSoundOn, setIsSoundOn] = useState(true);
+  const audioRef = useRef(new Audio(BgMusic1));
+
+  const startMusic = useCallback(() => {
+    if (isMusicOn) {
+      audioRef.current
+        .play()
+        .catch((error) => console.error("Error playing music:", error));
+    }
+  }, [isMusicOn]);
+
+  const toggleMusic = useCallback(() => {
+    setIsMusicOn((prevState) => {
+      if (!prevState) {
+        audioRef.current
+          .play()
+          .catch((error) => console.error("Error playing music:", error));
+      } else {
+        audioRef.current.pause();
+      }
+      return !prevState;
+    });
+  }, []);
+
+  const toggleSound = useCallback(() => {
+    setIsSoundOn((prevState) => !prevState);
+  }, []);
+
+  const value = {
+    isMusicOn,
+    isSoundOn,
+    toggleMusic,
+    toggleSound,
+    startMusic,
+  };
+
+  return (
+    <MusicContext.Provider value={value}>{children}</MusicContext.Provider>
+  );
+};
+
+export const useMusic = () => {
+  const context = useContext(MusicContext);
+  if (!context) {
+    throw new Error("useMusic must be used within a MusicProvider");
+  }
+  return context;
+};
