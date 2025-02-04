5df225abb098158bc87f58182c16cf0dab2342da
Vinay Shetye <vinay.courses.official@gmail.com>
2025-02-03 21:59:46 -0500

    updated file

--- src/pages/NameEntryPage.jsx
+++ src/pages/NameEntryPage.jsx
@@ -20,9 +20,9 @@ const NameEntryPage = () => {
   };
 
   return (
-    <NameEntryPageWrapper className="min-h-screen flex justify-center items-center bg-gradient-to-br from-green-400 via-green-600 to-green-800">
-      <div className="container p-10 text-center">
-        <h1 className="label_name text-7xl font-bold select-none text-white mb-5 tracking-wide drop-shadow-[3px_3px_0px_var(--tw-shadow-color)] shadow-green-800">
+    <NameEntryPageWrapper className="flex justify-center items-center min-h-screen bg-gradient-to-br from-green-400 via-green-600 to-green-800">
+      <div className="container p-2 text-center sm:p-2 md:p-8 lg:p-10">
+        <h1 className="label_name text-[2.3rem] sm:text-5xl md:text-6xl lg:text-7xl font-bold select-none text-white mb-0 sm:mb-2 md:mb-2 tracking-wide drop-shadow-[3px_3px_0px_var(--tw-shadow-color)] shadow-green-800">
           Enter Your Name
         </h1>
         <input
@@ -30,17 +30,22 @@ const NameEntryPage = () => {
           value={name}
           onChange={(e) => setName(e.target.value)}
           placeholder="Your Name"
-          className="name_field text-3xl my-8 text-teal-600 p-4 tracking-wider rounded-lg md:shadow-lg shadow-md focus:outline-none outline outline-0 focus:outline-0 border-0"
+          className="name_field text-xl sm:text-2xl md:text-3xl  
+             w-[240px] sm:w-[320px] md:w-[380px] lg:w-[450px] 
+             my-4 sm:my-6 md:my-8 text-teal-600 
+             p-3 sm:p-4 tracking-wider rounded-lg 
+             shadow-md focus:outline-none"
         />
+
         <button
-          className={`btn_start_game mt-4 ml-8 select-none bg-blue-700 text-4xl mx-auto text-white rounded-full shadow-lg transform transition-transform duration-1000 ${
+          className={`btn_start_game mt-2 sm:mt-3 md:mt-4 select-none bg-blue-700 text-2xl sm:text-3xl md:text-4xl mx-auto text-white rounded-full shadow-lg transform transition-transform duration-1000 ${
             animate ? "scale-15" : ""
           }`}
           onClick={handleStartGame}
           onMouseEnter={playHoverSound} // Play hover sound
         >
           <span
-            className={`btn_start_game-text bg-gradient-to-br from-blue-400 via-blue-500 to-blue-600 w-full block px-10 py-4 rounded-full -translate-y-1 hover:bg-gradient-to-tl active:-translate-y-0 transition-opacity duration-1000 ${
+            className={`btn_start_game-text bg-gradient-to-br from-blue-400 via-blue-500 to-blue-600 w-full block px-6 sm:px-8 md:px-10 py-2 sm:py-3 md:py-4 rounded-full -translate-y-1 hover:bg-gradient-to-tl active:-translate-y-0 transition-opacity duration-1000 ${
               animate ? "opacity-0" : "opacity-100"
             }`}
           >
