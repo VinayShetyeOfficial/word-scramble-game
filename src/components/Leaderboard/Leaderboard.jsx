fd3d0c6937c1e19136af768da7f86ba268818473
Vinay Shetye <vinay.courses.official@gmail.com>
2025-02-03 21:58:51 -0500

    updated file

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -11,8 +11,8 @@
 //   }, [topScores]);
 
 //   return (
-//     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-yellow-400 via-yellow-600 to-yellow-700">
-//       <h1 className="text-5xl font-bold text-white mb-8">Leaderboard</h1>
+//     <div className="flex flex-col justify-center items-center min-h-screen bg-gradient-to-br from-yellow-400 via-yellow-600 to-yellow-700">
+//       <h1 className="mb-8 text-5xl font-bold text-white">Leaderboard</h1>
 //       <ul className="text-3xl text-white">
 //         {scores.map((score, index) => (
 //           <li key={index} className="mb-2">
