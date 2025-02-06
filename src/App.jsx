4cce3c91aae2857823b21b95b21fb96301316ff8
Vinay Shetye <vinay.courses.official@gmail.com>
2025-02-05 23:05:11 -0500

    updated file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RouteGuard from "./components/RouteGuard/RouteGuard";
 import HomeScreen from "./components/HomeScreen/HomeScreen";
 import NameEntryPage from "./pages/NameEntryPage";
@@ -7,30 +7,31 @@ import LoadingScreen from "./pages/LoadingScreen";
 import PlayScreen from "./components/PlayScreen/PlayScreen";
 import Instructions from "./components/Instructions/Instructions";
 import Leaderboard from "./components/Leaderboard/Leaderboard";
+import GameProvider from "./contexts/GameContext";
 import { MusicProvider } from "./contexts/MusicContext";
-import { GameProvider } from "./contexts/GameContext";
 
-const App = () => {
+function App() {
   return (
     <GameProvider>
       <MusicProvider>
         <Router>
           <RouteGuard>
             <Routes>
-              {/* <Route path="/" element={<HomeScreen />} />
+              <Route path="/" element={<HomeScreen />} />
               <Route path="/enter-name" element={<NameEntryPage />} />
               <Route path="/load" element={<LoadingScreen />} />
               <Route path="/playScreen" element={<PlayScreen />} />
               <Route path="/instructions" element={<Instructions />} />
-              <Route path="/leaderboard" element={<Leaderboard />} /> */}
-
-              <Route path="/" element={<PlayScreen />} />
+              <Route path="/leaderboard" element={<Leaderboard />} />
             </Routes>
+            {/* <Routes>
+              <Route path="/" element={<LoadingScreen />} />
+            </Routes> */}
           </RouteGuard>
         </Router>
       </MusicProvider>
     </GameProvider>
   );
-};
+}
 
 export default App;
